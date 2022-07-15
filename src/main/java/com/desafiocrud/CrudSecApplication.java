be74187b51027a2b172e67cc5ab466e47dc76024
Eduardo Oliveira <adviou@gmail.com>
2022-07-15 09:18:10 -0300

    Project created

---
+++ src/main/java/com/desafiocrud/CrudSecApplication.java
@@ -0,0 +1,13 @@
+package com.desafiocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafiocrud/CrudSecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiocrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
